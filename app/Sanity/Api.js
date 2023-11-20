27e0a4067288eac8adfa2c009f4d1b0d366197b3
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-11-21 00:22:23 +0530

    initial commit

--- app/Sanity/Api.js
+++ app/Sanity/Api.js
@@ -1,10 +1,9 @@
 export const newsData = async () => {
     // Fetch data from external API
-    const res = await fetch("https://gxcmef9ngc.execute-api.ap-south-1.amazonaws.com/news/list?limit=20&flag=true", { headers: {
-      'X-API-KEY': "!@#trythis#@!"
-    } })
+    const res = await fetch("https://gxcmef9ngc.execute-api.ap-south-1.amazonaws.com/news/list?limit=20&flag=true")
     const result = await res.json()
-    const MainData = result.filter(result1 => (result1.main === "1"))
+    console.log(result)
+    const MainData = result?.filter(result1 => (result1.main === "1"))
     const SecondaryData = result.filter(result2 => (result2.main === "0" ))
     console.log("result@@@@@@@@@@@", result)
     return { MainData, SecondaryData};
