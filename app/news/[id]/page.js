cf6698e5b9f6e14a7e161e05bc1ae13c24410ec2
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-11-21 15:51:52 +0530

    News Detail and meta tag changes

--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { fetchNewsDetail } from "../../Sanity/Api";
 import NewsDetails from "../../components/NewsDetails"
-import MetaDocument from "@/app/components/MetaDocument";
 
 export default async function news({ params, searchParams }) {
   const fetchNewsDetails = await fetchNewsDetail(params.id)
@@ -14,12 +13,18 @@ export default async function news({ params, searchParams }) {
     title: fetchNewsDetails.title,
   }
     return <div>
-      <MetaDocument data={meta} />
+      {/* <MetaDocument data={meta} /> */}
       <meta name='keywords' content='Latest Tamil news, Latest SPorts news, big boss tamil, tamilnadu news, kollywood news, politiocs, tamil seithigal, trending' />
         <meta name='title' content={fetchNewsDetails.title}/>
         <meta name='og:image' content={fetchNewsDetails.image} />
-        <meta name='op:description' content={fetchNewsDetails.description} />
-        <meta name='op:url' content={fetchNewsDetails.url}/>
+        <meta name='og:description' content={fetchNewsDetails.description} />
+        <meta name='og:url' content={fetchNewsDetails.url}/>
+        <meta name="twitter:card" value="summary_large_image"/>
+      <meta name="twitter:site" value="@tamilnewsinfo"/>
+      <meta property="og:image:width" content="1070"/>
+      <meta property="og:image:height" content="580"/>
+      <meta name="mobile-web-app-capable" content="yes"/>
+      <meta content="text/html; charset=UTF-8" http-equiv="Content-Type"></meta>
       <NewsDetails data={fetchNewsDetails}/>
     </div>
 
