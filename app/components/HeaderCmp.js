c646ddf169f14392e3cd9b6410f319220c97beb9
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-11-23 18:40:35 +0530

    logo changes

--- app/components/HeaderCmp.js
+++ app/components/HeaderCmp.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 import { SocialIcon } from 'react-social-icons'
 const HeaderCmp = () => {
     return <div className="w-full z-20 md:h-12 md:flex md:justify-between fixed top-0 bg-white md:border-b md:border-gray-400 h-9 border-b border-gray-400">
-    <div className="m-4 -mt-1 md:mx-12 md:p-2 h-10 w-24">
+    <div className="m-4 -mt-1 md:mx-12 md:p-2 h-full w-full">
     <Link href="/">
-    <img src="https://i.ibb.co/VT0gnCb/Whats-App-Image-2023-11-14-at-9-43-18-PM.jpg" alt="logo" className="md:h-12 md:w-24 h-8 mt-1 md:mt-0 w-10" border="0" /></Link>
+    <img src="https://i.ibb.co/0JmSXKs/ed1ef1058adf.jpg" alt="ed1ef1058adf" border="0" className=" mt-1 md:mt-0 w-10" /></Link>
     </div>
     <div className="invisible sm:visible md:visible lg:visible">
         <ul className="md:flex md:p-2 md:mx-56 md:m-2 md:text-sm md:font-extrabold">
@@ -24,7 +24,7 @@ const HeaderCmp = () => {
             <li className="px-4  hover:text-yellow-500 cursor-pointer"><Link href="/cinema">சினிமா</Link></li>
         </ul>
     </div>
-    <div className="md:p-1 md:-mt-0 flex -mt-[78px] justify-end">
+    <div className="md:p-1 md:-mt-0  flex -mt-[71px] justify-end">
     <SocialIcon url="https://www.facebook.com/profile.php?id=61553449097172" style={{height:"25px", width:"30px"}}/>
     <SocialIcon url="https://twitter.com/reach2newsapp" style={{height:"25px", width:"30px"}} />
     <SocialIcon url="https://www.youtube.com/channel/UCvRdeCsdOspBsDoL6sITmNQ" style={{height:"25px", width:"30px"}} />
