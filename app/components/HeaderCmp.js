662a4eb1072cbce81938a3bb412e45282eb2c81f
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-11-23 16:24:24 +0530

    social media icons added

--- app/components/HeaderCmp.js
+++ app/components/HeaderCmp.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+import { SocialIcon } from 'react-social-icons'
 const HeaderCmp = () => {
-    return <div className="w-full md:h-16 md:flex md:justify-between md:border-b md:border-gray-400 h-9 border-b border-gray-400">
+    return <div className="w-full md:h-12 md:flex md:justify-between md:border-b md:border-gray-400 h-9 border-b border-gray-400">
     <div className="m-4 -mt-1 md:mx-12 md:p-2 h-10 w-24">
     <Link href="/">
-    <img src="https://i.ibb.co/VT0gnCb/Whats-App-Image-2023-11-14-at-9-43-18-PM.jpg" alt="logo" className="md:h-14 md:w-20 h-9 mt-1 md:mt-0 w-10" border="0" /></Link>
+    <img src="https://i.ibb.co/VT0gnCb/Whats-App-Image-2023-11-14-at-9-43-18-PM.jpg" alt="logo" className="md:h-12 md:w-20 h-8 mt-1 md:mt-0 w-10" border="0" /></Link>
     </div>
     <div className="invisible sm:visible md:visible lg:visible">
         <ul className="md:flex md:p-2 md:mx-56 md:m-2 md:text-sm md:font-extrabold">
@@ -22,6 +23,11 @@ const HeaderCmp = () => {
             <li className="px-4  hover:text-yellow-500 cursor-pointer"><Link href="/sports">விளையாட்டு</Link></li>
             <li className="px-4  hover:text-yellow-500 cursor-pointer"><Link href="/cinema">சினிமா</Link></li>
         </ul>
+    </div>
+    <div className="md:p-1 md:-mt-0 flex -mt-[85px] justify-end">
+    <SocialIcon url="https://www.facebook.com/profile.php?id=61553449097172" style={{height:"25px", width:"30px"}}/>
+    <SocialIcon url="https://twitter.com/reach2newsapp" style={{height:"25px", width:"30px"}} />
+    <SocialIcon url="https://www.youtube.com/channel/UCvRdeCsdOspBsDoL6sITmNQ" style={{height:"25px", width:"30px"}} />
     </div>
     </div>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 import Footer from "./components/Footer";
 import HeaderCmp from "./components/HeaderCmp";
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata = {
 //   title: 'Tamil News Info',
@@ -15,29 +15,33 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7330127866991302"
-     crossorigin="anonymous"></script>
-    <meta name="google-adsense-account" content="ca-pub-7330127866991302" />
-    
-      <body >
-      <NextTopLoader color="#FF0000"
-  initialPosition={2.0}
-  crawlSpeed={1000}
-  height={3}
-  crawl={true}
-  showSpinner={true}
-  easing="ease"
-  speed={1000}
-  shadow="0 0 10px #FF0000,0 0 5px #FF0000"
-  template='<div class="bar" role="bar"><div class="peg"></div></div> 
-  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
-  zIndex={1600}
-  showAtBottom={false} />
-      <HeaderCmp />
-      {children}
-      <Footer />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7330127866991302"
+        crossOrigin="anonymous"
+      />
+      <meta name="google-adsense-account" content="ca-pub-7330127866991302" />
+
+      <body>
+        <NextTopLoader
+          color="#FF0000"
+          initialPosition={2.0}
+          crawlSpeed={1000}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={1000}
+          shadow="0 0 10px #FF0000,0 0 5px #FF0000"
+          template="<div class=&quot;bar&quot; role=&quot;bar&quot;><div class=&quot;peg&quot;></div></div> 
+  <div class=&quot;spinner&quot; role=&quot;spinner&quot;><div class=&quot;spinner-icon&quot;></div></div>"
+          zIndex={1600}
+          showAtBottom={false}
+        />
+        <HeaderCmp />
+        {children}
+        <Footer />
       </body>
-      
     </html>
-  )
+  );
 }
