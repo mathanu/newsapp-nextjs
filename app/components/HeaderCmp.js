4b73378fda3f7fa9a32f59925cf7036d67292d5b
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-11-23 18:56:15 +0530

    responsive changes

--- app/components/HeaderCmp.js
+++ app/components/HeaderCmp.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Link from 'next/link';
 import { SocialIcon } from 'react-social-icons'
 const HeaderCmp = () => {
-    return <div className="w-full z-20 md:h-12 md:flex md:justify-between fixed top-0 bg-white md:border-b md:border-gray-400 h-9 border-b border-gray-400">
-    <div className="m-4 -mt-1 md:mx-12 md:p-2 h-full w-full">
+    return <div className="w-full z-20 md:h-12 md:flex md:justify-between fixed top-0 bg-white border-b border-gray-400 h-10">
+    <div className="m-2 -mt-1 md:mx-12 md:p-2 md:h-full md:w-full">
     <Link href="/">
-    <img src="https://i.ibb.co/0JmSXKs/ed1ef1058adf.jpg" alt="ed1ef1058adf" border="0" className=" mt-1 md:mt-0 w-10" /></Link>
+    <img src="https://i.ibb.co/0JmSXKs/ed1ef1058adf.jpg" alt="ed1ef1058adf"className=" mt-1 md:mt-0 w-10" /></Link>
     </div>
     <div className="invisible sm:visible md:visible lg:visible">
         <ul className="md:flex md:p-2 md:mx-56 md:m-2 md:text-sm md:font-extrabold">
@@ -17,14 +17,14 @@ const HeaderCmp = () => {
             <li className="px-4  hover:text-yellow-500 cursor-pointer"><Link href="/raasipalan">ராசிபலன்</Link></li>
         </ul>
     </div>
-    <div className="visible -mt-[166px] z-20 bg-white p-2 m-2 overflow-x-hidden sm:invisible md:invisible lg:invisible border-b border-gray-400">
+    <div className="visible -mt-[152px] z-20 font-bold bg-white p-2 m-2 overflow-x-hidden sm:invisible md:invisible lg:invisible border-b border-gray-400">
         <ul className="flex ">
             <li className="hover:text-yellow-500 cursor-pointer"><Link href="/">முகப்பு</Link></li>
             <li className="px-4  hover:text-yellow-500 cursor-pointer"><Link href="/sports">விளையாட்டு</Link></li>
             <li className="px-4  hover:text-yellow-500 cursor-pointer"><Link href="/cinema">சினிமா</Link></li>
         </ul>
     </div>
-    <div className="md:p-1 md:-mt-0  flex -mt-[71px] justify-end">
+    <div className="md:p-1 md:-mt-0  flex -mt-[80px] justify-end">
     <SocialIcon url="https://www.facebook.com/profile.php?id=61553449097172" style={{height:"25px", width:"30px"}}/>
     <SocialIcon url="https://twitter.com/reach2newsapp" style={{height:"25px", width:"30px"}} />
     <SocialIcon url="https://www.youtube.com/channel/UCvRdeCsdOspBsDoL6sITmNQ" style={{height:"25px", width:"30px"}} />
