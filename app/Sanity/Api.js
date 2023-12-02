fb8bdf45c08140792aa40fe98a61a19a6d037852
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-12-02 17:10:11 +0530

    read more added

--- app/Sanity/Api.js
+++ app/Sanity/Api.js
@@ -1,6 +1,6 @@
 export const newsData = async () => {
     // Fetch data from external API
-    const res = await fetch(process.env.LOCALAPI+"news/list?limit=13&flag=true", { cache: 'no-store' })
+    const res = await fetch(process.env.PRODAPI+"news/list?limit=13&flag=true", { cache: 'no-store' })
     const resultData = await res.json()
     let MainData = [];
     let SecondaryData= [] 
@@ -15,21 +15,21 @@ export const newsData = async () => {
   }
 
 export const fetchNewsDetail = async (id) => {
-  const data = await fetch(process.env.LOCALAPI+"news/fetchbyid?ID="+id, { cache: 'no-store' })
+  const data = await fetch(process.env.PRODAPI+"news/fetchbyid?ID="+id, { cache: 'no-store' })
   const result = await data.json();
   // console.log(result, id)
   return result;
 }
 
 export const fetchNewsList = async (slug) => {
-  const data = await fetch(process.env.LOCALAPI+"news/type?type="+slug, { cache: 'no-store' })
+  const data = await fetch(process.env.PRODAPI+"news/type?type="+slug, { cache: 'no-store' })
   const result = await data.json();
   console.log(result, slug)
   return result;
 }
 
 export const fetchHoroscope = async () => {
-  const data = await fetch(process.env.LOCALAPI+"horoscope/list")
+  const data = await fetch(process.env.PRODAPI+"horoscope/list")
   const result = await data.json();
   return result;
 }
