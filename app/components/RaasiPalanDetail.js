6e13058657286200bcb77dcb265a7d6de852216f
Mathan Jayapal [EXT] <mathan.jayapal@iff.com>
2023-12-07 00:14:58 +0530

    raasi palan changes

--- app/components/RaasiPalanDetail.js
+++ app/components/RaasiPalanDetail.js
@@ -6,7 +6,8 @@ const RaasiPalanDetail = (props) => {
       
     <div className="md:p-2 md:m-2 border shadow-inner p-2 m-4 md:flex">
       <div className="md:p-2 md:m-2 shadow-xl items-center md:w-3/12 md:ml-0 ml-10 md:mt-0 mt-6">
-        <img src="https://tamil.oneindia.com/img/2023/11/q166derm-down-1700135947.jpg" height={300} width={150}/> 
+        <img src={horoDetails?.image}/> 
+        <span className="ml-12 font-bold">{horoDetails?.name}</span>
       </div>
       <div className="p-2 m-2 shadow-xl md:w-9/12 md:font-normal">
       <h1>{horoDetails?.Daily}
